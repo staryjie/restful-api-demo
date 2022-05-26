a94d9204b7014c34808f8405e4ffadb57004e870
staryjie <staryjie@163.com>
2022-05-26 18:03:08 +0800

    修改host模块代码结构

---
+++ apps/host/interface.go
@@ -0,0 +1,17 @@
+package host
+
+import "context"
+
+// Host app service 的接口实现
+type Service interface {
+	// 录入主机
+	CreateHost(context.Context, *Host) (*Host, error)
+	// 查询主机列表
+	QueryHost(context.Context, *QueryHostRequest) (*HostSet, error)
+	// 查询主机详情
+	DescribeHost(context.Context, *QueryHostRequest) (*Host, error)
+	// 更新主机信息
+	UpdateHost(context.Context, *UpdateHostRequest) (*Host, error)
+	// 删除主机,前端需要展示被删除的主机信息，所以需要返回当前删除的主机信息
+	DeleteHost(context.Context, *DeleteHostRequest) (*Host, error)
+}
--- apps/host/model.go
+++ apps/host/model.go
@@ -1,7 +1,6 @@
 package host
 
 import (
-	"context"
 	"time"
 
 	"github.com/go-playground/validator/v10"
@@ -10,20 +9,6 @@ import (
 var (
 	validate = validator.New()
 )
-
-// Host app service 的接口实现
-type Service interface {
-	// 录入主机
-	CreateHost(context.Context, *Host) (*Host, error)
-	// 查询主机列表
-	QueryHost(context.Context, *QueryHostRequest) (*HostSet, error)
-	// 查询主机详情
-	DescribeHost(context.Context, *QueryHostRequest) (*Host, error)
-	// 更新主机信息
-	UpdateHost(context.Context, *UpdateHostRequest) (*Host, error)
-	// 删除主机,前端需要展示被删除的主机信息，所以需要返回当前删除的主机信息
-	DeleteHost(context.Context, *DeleteHostRequest) (*Host, error)
-}
 
 type HostSet struct {
 	Items []*Host `json:"items"`
