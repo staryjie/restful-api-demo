ae3e6c3cecb9b6006e524c48f2ed7b9bbbb5dd1c
staryjie <staryjie@163.com>
2022-05-27 16:51:37 +0800

    启动时初始化全局日志配置

--- cmd/start.go
+++ cmd/start.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/infraboard/mcube/logger/zap"
 	"github.com/spf13/cobra"
 
 	"github.com/staryjie/restful-api-demo/apps"
@@ -32,6 +35,11 @@ var StartCmd = &cobra.Command{
 			panic(err)
 		}
 
+		// 初始化全局日志Logger
+		if err := loadGlobalLogger(); err != nil {
+			return err
+		}
+
 		// 加载Host Service实体类
 		// service := impl.NewHostServiceImpl()
 
@@ -59,7 +67,79 @@ var StartCmd = &cobra.Command{
 	},
 }
 
+// 问题：
+//    1. HTTP API， GRPC API需要启动，消息总线也需要监听，比如负责注册配置，这些模块都是独立的
+//       都需要在程序启动的时候进行初始化和启动，如果都写在start中，那么start会很臃肿，难以维护
+//    2. 服务的优雅关闭怎么实现？ 外部会发生一个Terminalied（中断）信号给程序，程序需要监听并处理这些信号
+//       需要实现程序优雅关闭的处理逻辑：从外到内完成资源的释放和关闭处理
+//         1. API层的关闭
+//         2. 消息总线等关闭
+//         3. 关闭数据库连接
+//         4. 如果使用了注册中心，最后需要在注册中心完成服务下线操作
+//         5. 程序退出
+
 // 还没有初始化Logger实例
+// log 为全局变量, 只需要load 即可全局可用户, 依赖全局配置先初始化
+func loadGlobalLogger() error {
+	var (
+		logInitMsg string
+		level      zap.Level
+	)
+
+	// 根据配置文件加载后的conf对象来配置全局logger对象
+	lc := conf.C().Log
+
+	// 设置日志级别
+	// DebugLevel: "debug",
+	// InfoLevel:  "info",
+	// WarnLevel:  "warning",
+	// ErrorLevel: "error",
+	// FatalLevel: "fatal",
+	// PanicLevel: "panic",
+	lv, err := zap.NewLevel(lc.Level)
+	if err != nil {
+		// 配置出错则使用默认日志级别info
+		logInitMsg = fmt.Sprintf("%s, use default level INFO", err)
+		level = zap.InfoLevel
+	} else {
+		level = lv
+		logInitMsg = fmt.Sprintf("log level: %s", lv)
+	}
+
+	// 使用默认配置初始化logger全局配置
+	zapConfig := zap.DefaultConfig()
+
+	// 加上用户自定义配置完成全局logger配置
+	zapConfig.Level = level
+
+	// 配置程序是否在启动的时候创建新的日志文件
+	zapConfig.Files.RotateOnStartup = false
+
+	// 配置日志输出
+	switch lc.To {
+	case conf.ToStdout:
+		// 日志打印到标准输出
+		zapConfig.ToStderr = true
+		zapConfig.ToFiles = false
+	case conf.ToFile:
+		// 日志打印到文件
+		zapConfig.Files.Name = "api.log"
+		zapConfig.Files.Path = lc.PathDir
+	}
+
+	// 配置日志输出格式
+	switch lc.Format {
+	case conf.JSONFormat:
+		zapConfig.JSON = true
+	}
+
+	// 将配置应用到全局logger对象
+	if err := zap.Configure(zapConfig); err != nil {
+		return err
+	}
+	zap.L().Named("INIT").Info(logInitMsg)
+	return nil
+}
 
 func init() {
 	StartCmd.PersistentFlags().StringVarP(&confFile, "config", "f", "etc/demo.toml", "demo api配置文件路径")
